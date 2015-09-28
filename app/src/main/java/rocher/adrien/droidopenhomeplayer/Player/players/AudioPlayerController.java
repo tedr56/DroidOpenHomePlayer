6f5fb7708ab10a7461957a4c23fed666e41c19ea
Adrien Rocher Vj <a.rocher.vj@gmail.com>
2015-09-28 18:13:42 +0200

    Dummy Audio Controller

---
+++ app/src/main/java/rocher/adrien/droidopenhomeplayer/Player/players/AudioPlayerController.java
@@ -0,0 +1,15 @@
+package rocher.adrien.droidopenhomeplayer.Player.players;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import org.apache.log4j.Logger;
+
+import rocher.adrien.droidopenhomeplayer.Player.IPlayerController;
+import rocher.adrien.droidopenhomeplayer.Player.events.EventBase;
+import rocher.adrien.droidopenhomeplayer.Channel.ChannelBase;
+import rocher.adrien.droidopenhomeplayer.Utils.Config;
+/*
+public class AudioPlayerController extends Observable implements IPlayerController, Observer {
+}
+*/
